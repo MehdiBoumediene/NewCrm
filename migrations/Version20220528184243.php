dc4727d321026d900c8fa95aff52f811b464e840
Adiba-Boufeldja <boufeldja.adiba@ifde-learning.fr>
2022-05-28 19:58:57 +0100

    Backend: Création du systeme d'authentification - la finalisation du systeme

--- migrations/Version20220528184243.php
+++ migrations/Version20220528184243.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220526154822 extends AbstractMigration
+final class Version20220528184243 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,41 +2,51 @@
 
 namespace App\Controller;
 
-
 use App\Entity\User;
-use App\Form\RegisterType;
-use Doctrine\ORM\EntityManager;
+use App\Form\RegistrationFormType;
+use App\Security\UsersAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
-   
- 
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager ){
-        $this->entityManager = $entityManager;
-    }
-
-   
-    #[Route('/inscription', name: 'app_registration')]
-    public function index(Request $request): Response
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UsersAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
-
-
-        $user= new User();
-        $form=$this->createForm(RegisterType::class,$user);
-
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
-    
-      /*  if ($form->isSubmitted() && $form->isValid()) { 
-            
-        }*/
 
-        return $this->render('registration/index.html.twig');
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+            $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
     }
 }
