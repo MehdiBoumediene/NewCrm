7781b87140ed61ccae73e09e86f9d2bdfa76fadc
Adiba_Boufeldja <boufeldja.adiba@ifde-learning.fr>
2022-05-29 10:17:01 +0100

    Backend: Création d'un systeme d'authentification - L'ajout du champs rôle dans le Register Form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,18 +4,19 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -60,6 +61,21 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Étudiant' => 'ROLE_ETUDIANT',
+                    'Intervenant' => 'ROLE_INTERVENANT',
+                    'Tuteur' => 'ROLE_TUTEUR',
+                    'Administrateur' => 'ROLE_ADMIN',
+                    
+                ],
+                'expanded' => false,
+                'multiple' => true,
+                'required' => false,
+                'label' => 'Rôles' 
+            ])
+
 
             ;
     }
