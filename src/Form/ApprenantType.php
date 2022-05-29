aa2f5a2d520d3d7c8798a9d8ae9899f94d22bc89
Adiba_Boufeldja <boufeldja.adiba@ifde-learning.fr>
2022-05-29 14:37:47 +0100

    Backend : Création des crud pour les entités

---
+++ src/Form/ApprenantType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Apprenant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ApprenantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('adresse')
+            ->add('telephone')
+            ->add('email')
+            ->add('createdAt')
+            ->add('createdBy')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Apprenant::class,
+        ]);
+    }
+}
